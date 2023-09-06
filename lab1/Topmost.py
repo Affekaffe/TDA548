ef2586f2ce5c310eb847ab74d3497f7302450d5a
ViktorYeet <viktor@stefansbo.se>
2023-09-06 11:29:20 +0200

    Added test

--- lab1/Topmost.py
+++ lab1/Topmost.py
@@ -1,9 +1,8 @@
 import wordfreq
 #test = wordfreq.tokenize("abc, Abbbb det finns ett ord")
 #print(test)
-test = wordfreq.tokenize(input("Enter String: "))
-print(test)
-file = open("examples/article1.txt", encoding="utf-8")
+#test = wordfreq.tokenize(input("Enter String: "))
+#print(test)
+file = open("examples/test.txt")
 
-print(1)
 #gurka yoooo Ok whazzup
