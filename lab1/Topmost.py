9f087433ff0cae0eed7c7795d5b53e1b21746895
ViktorYeet <viktor@stefansbo.se>
2023-09-08 09:17:33 +0200

    topmost main now can read local files and wepages

--- lab1/Topmost.py
+++ lab1/Topmost.py
@@ -1,13 +1,34 @@
-import wordfreq
+import wordfreq, sys, urllib.request
 
 def fileToLineList(link):
     #open the file and read the contents line by line
     file = open(link)
-    lines = file.readlines()
+    data = file.read()
+    lines = data.split("\n")
     file.close()
     return lines
 
-lines2 = fileToLineList("lab1/examples/article1.txt")
-words2 = wordfreq.tokenize(lines2)
+def main():
+    #the lab is talking about som stopwords that should not be included and they have those on index 1
+    index = 1
+    lines = []
 
-print(words2)
+    #if no arguments were specified just exit the function
+    if(sys.argv.__len__() <= 1): return
+
+    #if we have a wev link read it
+    if(sys.argv[index].startswith("https://")or sys.argv[index].startswith("http://")):
+        respons = urllib.request.urlopen(sys.argv[index])
+        lines = respons.read().decode("utf8").splitlines()
+        
+    #if it's not from the web it's a local file
+    else:
+        lines = fileToLineList("lab1/" + sys.argv[index])
+    words = wordfreq.tokenize(lines)
+    print(words)
+
+main()
+#lines2 = fileToLineList("lab1/examples/article1.txt")
+#words2 = wordfreq.tokenize(lines2)
+#
+#print(words2)
