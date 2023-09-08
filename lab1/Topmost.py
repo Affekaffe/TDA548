81d70fe0920861d40ddd77c8e8f59d290effa01e
ViktorYeet <viktor@stefansbo.se>
2023-09-08 08:17:50 +0200

    gitignore works?

--- lab1/Topmost.py
+++ lab1/Topmost.py
@@ -1,9 +1,13 @@
 import wordfreq
-#test = wordfreq.tokenize("abc, Abbbb det finns ett ord")
-#print(test)
-test = wordfreq.tokenize(input("Enter String: "))
-print(test)
-file = open("examples/article1.txt", encoding="utf-8")
 
-print(1)
-#gurka
+def fileToLineList(link):
+    #open the file and read the contents line by line
+    file = open(link)
+    lines = file.readlines()
+    file.close()
+    return lines
+
+lines2 = fileToLineList("lab1/examples/article1.txt")
+words2 = wordfreq.tokenize(lines2)
+
+print(words2)
