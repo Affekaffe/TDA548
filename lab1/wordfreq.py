8ef5fe307da31044dbaf88cdad2ab8d2c298c4c0
Affekaffe <mattias.enell10@gmail.com>
2023-09-13 15:33:11 +0200

    comments

--- lab1/wordfreq.py
+++ lab1/wordfreq.py
@@ -1,36 +1,41 @@
 def tokenize(lines):
     words = []
-    for line in lines:
-        start = 0 
-        end  = 0 
-        while start < (len(line)):
-            while start < (len(line)) and line[start].isspace():
+    for line in lines: # loop over file
+        start = 0 # start of each word
+        end  = 0 # end of each word, to be determined by while loops below
+
+        while start < (len(line)): 
+            while start < (len(line)) and line[start].isspace(): # skip if space
                 start = start + 1
 
             if start < len(line):
 
-                end = start
-                if line[start].isalpha():
+                # while loops increases end until word is complete
+                
+                end = start # set start of new word
+                if line[start].isalpha(): # check if letters
                     while end<(len(line)) and line[end].isalpha():
                         end = end + 1
 
-                elif line[start].isdigit():
+                elif line[start].isdigit(): # check if digits
                     while end<(len(line)) and line[end].isdigit():
                         end = end + 1
-  
+                
+                # check if symbol, no while loop as symbols dont concatenate
                 elif end<(len(line)) and not (line[end].isdigit() or line[end].isalpha() or line[end].isspace()):
                     end = end + 1
 
-                words.append(line[start:end].lower())
+                words.append(line[start:end].lower()) # add word to list
                 start = end - 1
 
             start = start + 1
     return words
 
 def countWords(word_list, ignored_words):
-    relevant_words = [w for w in word_list if w not in ignored_words]
-    word_count = dict((word, 0) for word in set(relevant_words)) 
-    for word in relevant_words:
+    relevant_words = [w for w in word_list if w not in ignored_words] # new list without ignored_words
+    word_count = dict((word, 0) for word in set(relevant_words)) # init dict
+    
+    for word in relevant_words: # increase count in dict for each occurence
         word_count[word] += 1
     return word_count 
 
