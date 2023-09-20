dd398383af4c60362e4b6a77214bef6028f24a71
ViktorYeet <viktor@stefansbo.se>
2023-09-20 16:15:01 +0200

    did something, still works ready for evaluation

--- lab2/native_regression.py
+++ lab2/native_regression.py
@@ -1,24 +1,21 @@
-from numpy import *
+from matrix import *
 import sys
 import matplotlib.pyplot as plt
-
-def powers(nlist, n1, n2):
-    return array([[n**i for i in range(n1,n2+1)] for n in nlist]) # powers from n1 to n2 for each n in nlist
 
 def main():
 
     #open file and convert contents to matrix
     file = sys.argv[1]
     M = loadtxt(file)
-    X = [M[i][0] for i in range(0,len(M))]
-    Y = [M[i][1] for i in range(0,len(M))]
+    X = [M[i][0] for i, _ in enumerate(M)]
+    Y = [M[i][1] for i, _ in enumerate(M)]
 
     #linear regression
     Xp  = powers(X,0,1)
     Yp  = powers(Y,1,1)
     Xpt = transpose(Xp)
-    
-    [[b],[m]] = matmul(linalg.inv(matmul(Xpt,Xp)),matmul(Xpt,Yp)) #b,m are the predicted values
+
+    [[b],[m]] = matmul(invert(matmul(Xpt,Xp)),matmul(Xpt,Yp)) #b,m are the predicted values
 
     Y2 = [b + m*x for x in X]
 
@@ -26,5 +23,6 @@ def main():
     plt.plot(X,Y,"ro")
     plt.plot(X,Y2)
     plt.show()
+
 
 main()
